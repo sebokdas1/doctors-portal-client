961c47c68ac32201351e90b919bea4f348ff260d
sebokdas1 <sebok.das.ff@gmail.com>
2022-05-14 22:56:14 +0600

    nested

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 
 const Dashboard = () => {
     return (
         <div class="drawer drawer-mobile">
-            <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
-            <div class="drawer-content flex flex-col items-center justify-center">
-                {/* <!-- Page content here --> */}
-                <label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden">Open drawer</label>
-
+            <input id="dashboard-sidebar" type="checkbox" class="drawer-toggle" />
+            <div class="drawer-content">
+                <h2 className='text-2xl font-bold text-purple-500'>Welcome to your Dashboard</h2>
+                <Outlet></Outlet>
             </div>
             <div class="drawer-side">
-                <label for="my-drawer-2" class="drawer-overlay"></label>
-                <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
+                <label for="dashboard-sidebar" class="drawer-overlay"></label>
+                <ul class="menu p-4 overflow-y-auto w-48 bg-base-100 text-base-content">
                     {/* <!-- Sidebar content here --> */}
-                    <li><Link>Sidebar Item 1</Link></li>
-                    <li><Link to="/dashboard/review" >Sidebar Item 2</Link></li>
+                    <li><Link to="/dashboard">My Appointments</Link></li>
+                    <li><Link to="/dashboard/review">My Reviews</Link></li>
+                    {/* <li><Link to="/dashboard/history">My History</Link></li> */}
                 </ul>
 
             </div>
