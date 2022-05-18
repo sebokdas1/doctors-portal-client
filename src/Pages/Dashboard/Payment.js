71bb55a09eb00aa579ef1654f314b9ec1321308f
sebokdas1 <sebok.das.ff@gmail.com>
2022-05-18 12:35:40 +0600

    checkout

--- src/Pages/Dashboard/Payment.js
+++ src/Pages/Dashboard/Payment.js
@@ -1,12 +1,39 @@
+import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
+import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
+import Loading from '../Shared/Loading';
 
+const stripePromise = loadStripe('pk_test_51L0gJvIQnceSNo7BgQ3yMPfxaCnTGk52gUrBRBiHGrermMxTzKpwBoP8IsNTcrMcsVYvT1hYlCvrQ61OMzVhV0lt0011ffAS8O');
 const Payment = () => {
     const { id } = useParams();
+    const url = `http://localhost:5000/booking/${id}`;
+    const { data: appointment, isLoading } = useQuery(['booking', id], () => fetch(url, {
+        method: 'GET',
+        headers: {
+            'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+        }
+    }).then(res => res.json()))
+    if (isLoading) {
+        return <Loading />
+    }
     return (
         <div>
-            <h3>id: {id}</h3>
+            <div class="card w-50 max-w-md bg-base-100 shadow-xl my-12">
+                <div class="card-body">
+                    <p className='text-success font-bold'>Hello, {appointment.patientName}</p>
+                    <h2 class="card-title">Please Pay for {appointment.treatment}</h2>
+                    <p>Your appointment: <span className='text-orange-700'>{appointment.date}</span> at {appointment.slot}</p>
+                    <p>Please pay: ${appointment.price}</p>
+                </div>
+            </div>
+            <div class="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100">
+                <div class="card-body">
+
+                </div>
+            </div>
         </div>
+
     );
 };
 
