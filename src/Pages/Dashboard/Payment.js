4dab179861846a3ab577364425a23031c5e42071
sebokdas1 <sebok.das.ff@gmail.com>
2022-05-18 16:40:19 +0600

    setup card info

--- src/Pages/Dashboard/Payment.js
+++ src/Pages/Dashboard/Payment.js
@@ -1,8 +1,10 @@
+import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
 import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
 import Loading from '../Shared/Loading';
+import CheckoutForm from './CheckoutForm';
 
 const stripePromise = loadStripe('pk_test_51L0gJvIQnceSNo7BgQ3yMPfxaCnTGk52gUrBRBiHGrermMxTzKpwBoP8IsNTcrMcsVYvT1hYlCvrQ61OMzVhV0lt0011ffAS8O');
 const Payment = () => {
@@ -29,7 +31,9 @@ const Payment = () => {
             </div>
             <div class="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100">
                 <div class="card-body">
-
+                    <Elements stripe={stripePromise}>
+                        <CheckoutForm />
+                    </Elements>
                 </div>
             </div>
         </div>
